0dc16fbf685f4cf704e5e61d57091b989cd73d4c
“kat09tails” <kathryn.reese@umontana.edu>
2019-02-05 14:58:57 -0700

    first commit

---
+++ group.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void KatRInfo(){
+   printf("Please Complete KatRInfo\n");
+}
+
+void AlecRichardsonInfo(){
+   printf("Please Complete AlecRichardsonInfo\n");
+}
+
+void MarchusRichlieInfo(){
+   printf("Please Complete MarchusRichlieInfo\n");
+}
+
+void EthanRocheleauInfo(){
+   printf("Please Complete EthanRocheleauInfo\n");
+}
+
+void JanRoddyInfo(){
+   printf("Please Complete JanRoddyInfo\n");
+}
+
+
+int main(void){
+   printf("Our group name is: GroupR\n");
+   printf("These are our members:\n");
+   printf("Kathryn Reese\n");
+   printf("Alec Richardson\n");
+   printf("Marcus Richlie\n");
+   printf("Jan Roddy\n");
+
+   printf("\n");
+
+   KatRInfo();
+   AlecRichardsonInfo();
+   MarchusRichlieInfo();
+   EthanRocheleauInfo();
+   JanRoddyInfo();
+
+   return 0;
+}
